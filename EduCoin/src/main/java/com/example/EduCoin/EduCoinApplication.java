1ceb0632090ea935f22b5e087295ddc54f140a83
DaviSantos23 <78852206+DaviSantos23@users.noreply.github.com>
2025-05-07 19:43:44 -0300

    Create Back-End(Spring Boot)
    Create Back-End(Spring Boot)

---
+++ EduCoin/src/main/java/com/example/EduCoin/EduCoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.EduCoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduCoinApplication.class, args);
+	}
+
+}
